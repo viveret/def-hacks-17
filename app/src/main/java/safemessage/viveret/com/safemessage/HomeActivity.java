ca5625a99451c45f2639dc0f4fc8493608cfc98a
viveret <viveret.amant@gmail.com>
2017-01-14 18:04:10 -0800

    fixed a few things

--- app/src/main/java/safemessage/viveret/com/safemessage/HomeActivity.java
+++ app/src/main/java/safemessage/viveret/com/safemessage/HomeActivity.java
@@ -1,16 +1,26 @@
-package safemessage.viveret.com.safemessage.view;
+package safemessage.viveret.com.safemessage;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.ListView;
-
-import safemessage.viveret.com.safemessage.R;
 
 /**
  * Created by Amy on 1/14/2017.
  */
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
 
     /*
     This method expands and hides the "block list" drawer located in fragment_home.
--- app/src/main/java/safemessage/viveret/com/safemessage/LoginActivity.java
+++ app/src/main/java/safemessage/viveret/com/safemessage/LoginActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import safemessage.viveret.com.safemessage.view.HomeActivity;
-
 public class LoginActivity extends AppCompatActivity {
     // private CallbackManager callbackManager;
 
