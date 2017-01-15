0048980250ad25189b025d30069d266517d8700b
Amy Irving <amy.eli.irving@gmail.com>
2017-01-14 23:23:29 -0800

    Created DataProvider that is responsible for the info in the drop down list in the navigation drawer.  Made ExpListAdapter that costumizes the drop down list.

--- app/src/main/java/safemessage/viveret/com/safemessage/HomeActivity.java
+++ app/src/main/java/safemessage/viveret/com/safemessage/HomeActivity.java
@@ -11,10 +11,12 @@ import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ExpandableListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-import safemessage.viveret.com.safemessage.sms.SMSData;
 import safemessage.viveret.com.safemessage.sms.SMSFactory;
 import safemessage.viveret.com.safemessage.view.HomeFragment;
 
@@ -34,18 +36,32 @@ public class HomeActivity extends Activity
 
     private Fragment myFrag;
 
+    private HashMap<String, List<String>> expandableList;
+    private List<String> subList;
+    private ExpandableListView expList;
+    private ExpListAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         allSms = new SMSFactory(this, this);
+        //Initialize expList
+        expList = (ExpandableListView) findViewById(R.id.expandable_list);
+        expandableList = DataProvider.getInfo();
+        subList = new ArrayList<String>(expandableList.keySet());
+        adapter = new ExpListAdapter(this, expandableList, subList);
+        expList.setAdapter(adapter);
+
+
+        allSms = AllSMSLoader.getSMS(this);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(
                 this,                  /* host Activity */
                 mDrawerLayout,         /* DrawerLayout object */
-                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
+                R.mipmap.ic_launcher,  /* nav drawer icon to replace 'Up' caret */
                 R.string.drawer_open,  /* "open drawer" description */
                 R.string.drawer_close  /* "close drawer" description */
         ) {
@@ -109,10 +125,5 @@ public class HomeActivity extends Activity
         // Handle your other action bar items...
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onSmsUpdated(List<SMSData> newSet) {
-
     }
 }
